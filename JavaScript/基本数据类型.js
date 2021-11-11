26d6b669743128691913f78e90a55b716427253c
chenhaoya <1627785181@qq.com>
2021-11-11 14:45:57 +0800

    a

---
+++ JavaScript/基本数据类型.js
@@ -0,0 +1,13 @@
+var a=1;
+var b="JavaScript";
+var c=1.1;
+var d;
+var e=true;
+var f=null;
+
+document.write(a+"<br />");
+document.write(b+"<br />");
+document.write(c+"<br />");
+document.write(d+"<br />");
+document.write(e+"<br />");
+document.write(f+"<br />");
